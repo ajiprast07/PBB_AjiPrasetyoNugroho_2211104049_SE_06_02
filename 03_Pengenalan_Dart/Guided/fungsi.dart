341bcea43411da0bd2514debb67c78c463984f64
ajiprast07 <136548622+ajiprast07@users.noreply.github.com>
2024-10-02 20:04:26 +0700

    Add files via upload

---
+++ 03_Pengenalan_Dart/Guided/fungsi.dart
@@ -0,0 +1,16 @@
+// Fungsi tanpa nilai kembalian
+/*void cetakPesan(String pesan) {
+  print(pesan);
+}
+
+// Fungsi dengan nilai kembalian
+int perkalian(int a, int b) {
+  return a * b;
+}
+
+void main() {
+  int hasil = perkalian(5, 4);
+  print('hasil perkalian fungsi: $hasil');
+
+  cetakPesan('Halo, Guys!');
+}*/
